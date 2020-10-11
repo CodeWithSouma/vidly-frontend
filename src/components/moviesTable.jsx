b0530b336d21c5e71f969a81a06624575e98fccb
Souma <souma.hitech@gmail.com>
2020-10-11 21:20:56 +0530

    extract movie table and put into a seperate component

---
+++ src/components/moviesTable.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Heart from "./common/heart";
+
+const MoviesTable = (props) => {
+  const { movies, onHeartClick, onDelete } = props;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Stock</th>
+          <th>Rate</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie) => (
+          <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td>
+              <Heart onClick={() => onHeartClick(movie)} liked={movie.liked} />
+            </td>
+            <td>
+              <button
+                onClick={() => onDelete(movie)}
+                className="btn btn-danger btn-sm"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default MoviesTable;
