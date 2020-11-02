2a9912384dcac8661243e1e3939f87b8e7e730d4
Souma <souma.hitech@gmail.com>
2020-11-02 13:03:08 +0530

    hide new button for unauthorised user

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {toast} from 'react-toastify';
+import { toast } from "react-toastify";
 import { getMovies, deleteMovie } from "../services/movieService";
 import { getGenres } from "../services/genreService";
 import MoviesTable from "./moviesTable";
@@ -16,16 +16,16 @@ class Movies extends Component {
     genres: [],
     pageSize: 4,
     currentPage: 1,
-    searchQuery:"",
-    selectedGenre:null,
+    searchQuery: "",
+    selectedGenre: null,
     sortColumn: { path: "title", order: "asc" },
   };
 
   async componentDidMount() {
-    const {data} = await getGenres();
+    const { data } = await getGenres();
     const genres = [{ _id: "", name: "All Genres" }, ...data];
-    const {data: movies} = await getMovies();
-    this.setState({ movies , genres });
+    const { data: movies } = await getMovies();
+    this.setState({ movies, genres });
   }
 
   handleDelete = async (movie) => {
@@ -33,12 +33,12 @@ class Movies extends Component {
     const movies = originalMovies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
 
-    try{
+    try {
       await deleteMovie(movie._id);
-    }catch(ex){
-      if(ex.response && ex.response.status === 404)
-        toast.error('This movie has already been deleted.');
-      this.setState({movies:originalMovies});
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        toast.error("This movie has already been deleted.");
+      this.setState({ movies: originalMovies });
     }
   };
 
@@ -55,12 +55,12 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    this.setState({ selectedGenre: genre, searchQuery:"",currentPage: 1 });
+    this.setState({ selectedGenre: genre, searchQuery: "", currentPage: 1 });
   };
 
-  handleSearch = query => {
-    this.setState({searchQuery:query,selectedGenre:null,currentPage:1})
-  }
+  handleSearch = (query) => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
+  };
 
   handleSort = (sortColumn) => {
     this.setState({ sortColumn });
@@ -73,14 +73,18 @@ class Movies extends Component {
       movies: allMovies,
       selectedGenre,
       sortColumn,
-      searchQuery
+      searchQuery,
     } = this.state;
 
     let filtered = allMovies;
-    if(searchQuery)
-      filtered = allMovies.filter(m => m.title.toLowerCase().startsWith(searchQuery.toLowerCase()));
-    else if(selectedGenre && selectedGenre._id)
-        filtered = allMovies.filter((movie) => movie.genre._id === selectedGenre._id);        
+    if (searchQuery)
+      filtered = allMovies.filter((m) =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+    else if (selectedGenre && selectedGenre._id)
+      filtered = allMovies.filter(
+        (movie) => movie.genre._id === selectedGenre._id
+      );
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
@@ -97,8 +101,9 @@ class Movies extends Component {
       genres,
       selectedGenre,
       sortColumn,
-      searchQuery
+      searchQuery,
     } = this.state;
+    const { user } = this.props;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
@@ -114,15 +119,17 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <Link
-            to="/movies/new"
-            className="btn btn-primary"
-            style={{ marginBottom: 20 }}
-          >
-            New movie
-          </Link>
+          {user && (
+            <Link
+              to="/movies/new"
+              className="btn btn-primary"
+              style={{ marginBottom: 20 }}
+            >
+              New movie
+            </Link>
+          )}
           <p>Showing {totalCount} movies in the database.</p>
-          <SearchBox value={searchQuery} onChange={this.handleSearch}/>
+          <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
             movies={movies}
             sortColumn={sortColumn}
