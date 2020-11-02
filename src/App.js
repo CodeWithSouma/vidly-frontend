a8750d99dc11516a3c575bbb7fe85ba92040e1d0
Souma <souma.hitech@gmail.com>
2020-11-02 15:35:32 +0530

    refactor code

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
 import Logout from "./components/logout";
 import auth from "./services/authService";
+import ProtectedRoute from "./components/common/protectedRoute";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
@@ -33,11 +34,7 @@ class App extends Component {
             <Route path="/register" component={RegisterForm}></Route>
             <Route path="/logout" component={Logout}></Route>
             <Route path="/login" component={LoginForm}></Route>
-            <Route path="/movies/:id" 
-              render={props =>{
-                if(!user) return <Redirect to="/login"/>
-                return <MovieForm {...props}/>
-              }}></Route>
+            <ProtectedRoute path="/movies/:id" component={MovieForm}/>
             <Route
               path="/movies"
               render={(props) => <Movies {...props} user={user} />}
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Heart from "./common/heart";
 import Table from "./common/table";
+import Auth from "../services/authService";
+import authService from "../services/authService";
 
 class MoviesTable extends Component {
   columns = [
@@ -24,18 +26,27 @@ class MoviesTable extends Component {
         />
       ),
     },
-    {
-      key: "delete",
-      content: (movie) => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className="btn btn-danger btn-sm"
-        >
-          Delete
-        </button>
-      ),
-    },
   ];
+
+  deleteColumn = {
+    key: "delete",
+    content: (movie) => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className="btn btn-danger btn-sm"
+      >
+        Delete
+      </button>
+    ),
+  };
+
+  constructor() {
+    super();
+    const user = authService.getCurrentUser();
+    if (user && user.isAdmin) {
+      this.columns.push(this.deleteColumn);
+    }
+  }
 
   render() {
     const { movies, sortColumn, onSort } = this.props;
